be557aaac8e78985481d9bc8609bde015a1b8977
iZsk <sk.zeeshan1992@gmail.com>
2019-05-08 09:51:00 -0400

    latest list item page

---
+++ src/itempage.jsx
@@ -0,0 +1,16 @@
+import "./main.css";
+import "./style.css";
+import React, { Component } from "react";
+
+class Itempage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render = () => {
+    return <div>hello</div>;
+  };
+}
+
+export default Itempage;
